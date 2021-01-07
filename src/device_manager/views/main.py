aae2cd6f86cd79215bfe2421de64a3e3f98d166e
yuangezhizao-MacMini <yuangezhizao@icloud.com>
2021-01-07 20:41:19 +0800

    ✨ Feat(main.py): 转移路由、管理路由

--- src/device_manager/views/main.py
+++ src/device_manager/views/main.py
@@ -8,10 +8,31 @@
 """
 import flask
 
+from device_manager.models.device import Device
+
 app = flask.current_app
 bp = flask.Blueprint('main', __name__)
 
 
 @bp.route('/')
 def site_index():
-    return 'index'
+    return flask.render_template('index.html')
+
+
+@bp.route('/transfer/index')
+def transfer_index():
+    transfer_index_data = Device.query.order_by(Device.id.asc()).all()
+    return flask.render_template('transfer/index.html', transfer_index_data=transfer_index_data)
+
+
+@bp.route('/transfer/device')
+def transfer_device():
+    serial = flask.request.args.get('serial')
+    transfer_device_data = Device.query.filter_by(serial=serial).first_or_404()
+    return flask.render_template('transfer/device.html', transfer_device_data=transfer_device_data)
+
+
+@bp.route('/manage/index')
+def manage_index():
+    manage_index_data = Device.query.order_by(Device.id.asc()).all()
+    return flask.render_template('manage/index.html', manage_index_data=manage_index_data)
