3310ba098a62564e773cd322a59b3ab19518a095
yuangezhizao-MacMini <yuangezhizao@icloud.com>
2021-01-07 10:45:44 +0800

    🔧 Config(/instance): 数据库配置项

---
+++ src/instance/development.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env/ python3
+# -*- coding: utf-8 -*-
+"""
+    :Author: yuangezhizao
+    :Time: 2021/1/7 10:10
+    :Site: https://www.yuangezhizao.cn
+    :Copyright: © 2020~2021 yuangezhizao <root@yuangezhizao.cn>
+"""
+SQLALCHEMY_BINDS = {
+    'alsi': 'mysql+pymysql://<rm>:<rm>@10.0.2.11/alsi',
+}
