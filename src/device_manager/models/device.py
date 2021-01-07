b5ce1232266be1ae1f27ca1b86d3d171638964aa
yuangezhizao-MacMini <yuangezhizao@icloud.com>
2021-01-07 11:03:25 +0800

    🗃  DB(device.py): device Model

--- src/device_manager/models/device.py
+++ src/device_manager/models/device.py
@@ -7,3 +7,31 @@
     :Copyright: © 2020~2021 yuangezhizao <root@yuangezhizao.cn>
 """
 from device_manager.plugins.extensions import db
+
+
+class Device(db.Model):
+    __bind_key__ = 'alsi'
+    __tablename__ = 'device'
+
+    id = db.Column(db.Integer, primary_key=True)
+    type = db.Column(db.Integer)  # 设备类型
+    serial = db.Column(db.VARCHAR(50), nullable=False)  # 设备序列号
+    customer = db.Column(db.VARCHAR(50), nullable=False)  # 客户所有物
+    now_person = db.Column(db.Integer)  # 当前所有者
+    next_person = db.Column(db.Integer)  # 下个所有者
+    # TODO：所有者存储是否足够
+    person_status = db.Column(db.Integer)  # 所有者状态
+    device_status = db.Column(db.Integer)  # 设备状态
+    # TODO：关联表
+    tips = db.Column(db.VARCHAR(255))  # 备注
+    insert_time = db.Column(db.DateTime)
+    update_time = db.Column(db.DateTime)
+
+    def save(self):
+        db.session.add(self)
+        db.session.commit()
+        return self
+
+    def update(self):
+        db.session.commit()
+        return self
