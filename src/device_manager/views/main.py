35223eb302ffcc59186b47425e9fb4a6e2e0355e
yuangezhizao-workaccount <chenyuan.gao@alsi.cn>
2021-01-08 17:51:33 +0800

    设备传输路由、设备详情页面

--- src/device_manager/views/main.py
+++ src/device_manager/views/main.py
@@ -18,13 +18,20 @@ bp = flask.Blueprint('main', __name__)
 
 @bp.route('/')
 def site_index():
+    # from device_manager.plugins.extensions import db
+    # db.create_all()
+    if current_user.is_authenticated:
+        my_now_device_data = Device.query.filter_by(now_person=current_user).order_by(Device.id.asc()).all()
+        my_next_device_data = Device.query.filter_by(next_person=current_user).order_by(Device.id.asc()).all()
+        return flask.render_template('index.html', my_now_device_data=my_now_device_data,
+                                     my_next_device_data=my_next_device_data)
     return flask.render_template('index.html')
 
 
 @bp.route('/auth/index', methods=['GET', 'POST'])
 def auth_index():
     if current_user.is_authenticated:
-        return flask.redirect(flask.url_for('bp.site_index'))
+        return flask.redirect(flask.url_for('main.site_index'))
     if flask.request.method == 'POST':
         username = flask.request.form.get('username')
         user = User.query.filter_by(username=username).first()
@@ -35,7 +42,7 @@ def auth_index():
         else:
             remember = flask.request.form.get('remember', False)
             login_user(user, remember)
-            return flask.redirect(flask.url_for('bp.site_index'))
+            return flask.redirect(flask.url_for('main.site_index'))
     return flask.render_template('auth/index.html')
 
 
@@ -44,23 +51,40 @@ def auth_index():
 def auth_logout():
     logout_user()
     flask.flash('注销成功！', 'info')
-    return flask.redirect(flask.url_for('bp.auth_index'))
+    return flask.redirect(flask.url_for('main.auth_index'))
 
 
-@bp.route('/transfer/index')
-def transfer_index():
-    transfer_index_data = Device.query.order_by(Device.id.asc()).all()
-    return flask.render_template('transfer/index.html', transfer_index_data=transfer_index_data)
-
-
-@bp.route('/transfer/device')
+@bp.route('/transfer/device', methods=['GET', 'POST'])
+@login_required
 def transfer_device():
     serial = flask.request.args.get('serial')
     transfer_device_data = Device.query.filter_by(serial=serial).first_or_404()
-    return flask.render_template('transfer/device.html', transfer_device_data=transfer_device_data)
+    if flask.request.method == 'POST':
+        func = flask.request.form.get('func')
+        if func == 'start_transfer':
+            if transfer_device_data.now_person != current_user:
+                return '这已经不是你的设备了'
+            next_user_id = flask.request.form.get('next_user')
+            next_user = User.query.filter_by(username=next_user_id).first_or_404()
+            transfer_device_data.next_person = next_user
+            transfer_device_data.save()
+            return flask.redirect(flask.url_for('main.transfer_device', serial=serial))
+        elif func == 'stop_transfer':
+            transfer_confirm = flask.request.form.get('transfer_confirm')
+            if transfer_confirm:
+                if transfer_device_data.next_person != current_user:
+                    return '此设备并不需要你进行确认'
+                next_user = transfer_device_data.next_person
+                transfer_device_data.now_person = next_user
+                transfer_device_data.next_person = None
+                transfer_device_data.save()
+                return flask.redirect(flask.url_for('main.transfer_device', serial=serial))
+    user_all = User.query.order_by(User.id.asc()).all()
+    return flask.render_template('transfer/device.html', transfer_device_data=transfer_device_data, user_all=user_all)
 
 
 @bp.route('/manage/index')
+@login_required
 def manage_index():
     manage_index_data = Device.query.order_by(Device.id.asc()).all()
     return flask.render_template('manage/index.html', manage_index_data=manage_index_data)
