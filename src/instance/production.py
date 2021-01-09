db2e74a3b3f74db0683a8bc79240cbd64c15cf5d
yuangezhizao-workaccount <chenyuan.gao@alsi.cn>
2021-01-09 15:29:37 +0800

    sqlite 绝对路径对应更改

--- src/instance/production.py
+++ src/instance/production.py
@@ -8,5 +8,5 @@
 """
 SQLALCHEMY_BINDS = {
     # 'alsi': 'mysql+pymysql://<rm>:<rm>@10.0.2.11/alsi',
-    'alsi': 'sqlite:///D:\Apache24\htdocs\device_manager\src\device_manager\device_manger.db'
+    'alsi': 'sqlite:///D:\Apache24\htdocs\device_manager\device_manager\device_manger.db'
 }
