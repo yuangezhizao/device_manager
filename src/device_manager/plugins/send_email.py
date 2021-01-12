2cb8b8853bd31b04fc38da361928fb9c3980bd01
yuangezhizao-workaccount <chenyuan.gao@alsi.cn>
2021-01-12 13:45:45 +0800

    尝试修复邮件发送

--- src/device_manager/plugins/send_email.py
+++ src/device_manager/plugins/send_email.py
@@ -23,7 +23,7 @@ def send_transfer_email(serial, recipients, cc):
     msg = Message('CANoe 设备转移开始提醒',
                   sender=('ALSI ES CANoe 设备管理系统', 'chenyuan.gao@alsi.cn'),
                   recipients=[recipients],
-                  cc=cc)
+                  cc=[cc])
     msg.body = f'序列号为【{serial}】的 CANoe 设备开始转移，请及时确认！详情请参照：http://10.30.10.216:5000/transfer/device?serial={serial}'
     app = flask.current_app._get_current_object()
     thread = Thread(target=async_send_mail, args=(app, msg))
@@ -34,7 +34,7 @@ def send_confirm_email(serial, recipients, cc):
     msg = Message('CANoe 设备转移结束提醒',
                   sender=('ALSI ES CANoe 设备管理系统', 'chenyuan.gao@alsi.cn'),
                   recipients=[recipients],
-                  cc=cc)
+                  cc=[cc])
     msg.body = f'序列号为【{serial}】的 CANoe 设备已转移结束。详情请参照：http://10.30.10.216:5000/transfer/device?serial={serial}'
     app = flask.current_app._get_current_object()
     thread = Thread(target=async_send_mail, args=(app, msg))
